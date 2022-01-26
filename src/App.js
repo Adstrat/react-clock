9a81953577321dfc8d4453270e9b2e1706e28068
Adstrat <73385073+Adstrat@users.noreply.github.com>
2022-01-26 17:53:52 +0100

    clock centralised for all screen sizes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <h1 className="clock">{this.state.date.toLocaleTimeString()}</h1>
+      <div className="container">
+        <h1 className="clock">{this.state.date.toLocaleTimeString()}</h1>
+      </div>
     )
   }
 }
