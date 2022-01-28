00f21081ee25e754d206db6ef88b594c6b94d023
Adstrat <73385073+Adstrat@users.noreply.github.com>
2022-01-28 16:42:19 +0100

    clock component added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import './App.css';
 import React from 'react';
+import Clock from './Clock';
 
 export default class App extends React.Component {
-
   constructor( props ) {
     super( props );
     this.state = {
-      date: new Date(),
       color: '#39D1B4',
-      background: '#ebf4e8'
+      background: '#ebf4e8',
+      font: '400%'
     };
   }
 
   componentDidMount() {
-    setInterval( () => {
+    this.timerID = setInterval( () => {
       this.setState( { date: new Date() } );
     }, 1000 );
+  }
+
+  componentWillUnmount() {
+    clearInterval( this.timerID );
   }
 
   render() {
     return (
       <div className="main-container">
         <div className="time-container">
-          <h1
-            className="clock"
+          <Clock
             style={{
               color: this.state.color,
-              backgroundColor: this.state.background
+              backgroundColor: this.state.background,
+              fontSize: this.state.font
             }}
-          >
-            {this.state.date.toLocaleTimeString()}
-          </h1>
+          />
         </div>
       </div >
     )
