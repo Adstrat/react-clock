4be0957dc3a9a8ea57ef80cd094f90957dfa53bc
Adstrat <73385073+Adstrat@users.noreply.github.com>
2022-02-19 10:28:04 +0100

    created DateAndClock component
    separated 'date and clock' and 'timer' components

--- src/components/App.js
+++ src/components/App.js
@@ -1,93 +1,25 @@
 import React from 'react';
-import Clock from './Clock';
-import Date from './Date';
+
+import DateAndClock from './DateAndClock';
 import Timer from './Timer';
 
 export default class App extends React.Component {
-  constructor( props ) {
-    super( props );
-    this.state = {
-      color: '#39D1B4',
-      clock: true,
-      date: false
-    };
-    this.handleDateClick = this.handleDateClick.bind( this );
-    this.handleTimeClick = this.handleTimeClick.bind( this );
-    this.handleGreen = this.handleGreen.bind( this );
-    this.handleBlue = this.handleBlue.bind( this );
-    this.handleRed = this.handleRed.bind( this );
-  }
 
-  handleDateClick() {
-    this.setState( { clock: false, date: true } )
-  }
-
-  handleTimeClick() {
-    this.setState( { clock: true, date: false } )
-  }
-
-  handleGreen() {
-    this.setState( { color: '#39D1B4' } )
-  }
-
-  handleBlue() {
-    this.setState( { color: '#487BA0' } )
-  }
-
-  handleRed() {
-    this.setState( { color: '#D84A51' } )
-  }
 
   render() {
     return (
-
       <div className="main-container">
-        <div className="button-container">
-          <button
-            className="time-button"
-            onClick={this.handleTimeClick}
-          >Show Time
+
+        <div className='button-container'>
+          <button >
+            Date and Time
           </button>
-          <button
-            className="date-button"
-            onClick={this.handleDateClick}
-          >Show Date
+          <button className="button" >
+            Timer
           </button>
         </div>
 
-        <div className="time-container">
-          {this.state.clock &&
-            <Clock
-              style={{
-                color: this.state.color,
-              }}
-            />
-          }
-          {this.state.date &&
-            <Date
-              style={{
-                color: this.state.color,
-              }}
-            />
-          }
-        </div>
-
-
-        <div className="color-container">
-          <div
-            className="color green"
-            onClick={this.handleGreen}>
-          </div>
-          <div
-            className="color blue"
-            onClick={this.handleBlue}>
-          </div>
-          <div
-            className="color red"
-            onClick={this.handleRed}>
-          </div>
-        </div>
-
+        <DateAndClock />
         <Timer />
 
       </div >
